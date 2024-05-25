838f736c2b1317aadf4241cbe956261584c00231
softmaxer <sriram.vadlamani@proton.me>
2024-05-25 15:50:52 +0200

    Add endpoint to download one track from a session (for now)

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
-from files import up
+from files import up, down
 from chat import chat
 from session import Session
 
@@ -18,6 +18,7 @@ app.add_middleware(
 
 
 app.include_router(up.router)
+app.include_router(down.router)
 app.include_router(chat.router)
 
 
