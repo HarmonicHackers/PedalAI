b2eea10399b09d8a02e3047a9c70a574356f87c2
Verhille <titouan.verhille@epita.fr>
2024-05-25 17:05:04 +0200

    use mistral lib directly and force function calling

--- agents.py
+++ agents.py
@@ -1,86 +1,132 @@
-from langchain_core.tools import tool
-from langchain_groq import ChatGroq
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.agents import AgentExecutor, create_tool_calling_agent
-from langchain import hub
-from langchain_mistralai import ChatMistralAI
-
-from pedalboard import Pedalboard
-from pedalboard import Reverb, Gain, Bitcrush
-
+from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage
 import os
 
+model = "mistral-large-latest"
 api_key = os.environ.get("MISTRAL_API_KEY")
-if not api_key:
-    raise ValueError("MISTRAL_API_KEY environment variable not set.")
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "reverb_effect",
+            "description": "Add a reverb effect to the audio.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "room_size": {
+                        "type": "number",
+                        "description": "The room size, between 0 and 1.",
+                    },
+                    "damping": {
+                        "type": "number",
+                        "description": "The damping.",
+                    },
+                    "wet_level": {
+                        "type": "number",
+                        "description": "The wet level.",
+                    },
+                    "dry_level": {
+                        "type": "number",
+                        "description": "The dry level.",
+                    },
+                    "freeze_mode": {
+                        "type": "number",
+                        "description": "The freeze mode.",
+                    },
+                },
+                "required": ["room_size"],
+            },
+        },
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "bitcrush_effect",
+            "description": "Add a bitcrush effect to the audio.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "bit_depth": {
+                        "type": "number",
+                        "description": "The bit depth.",
+                    }
+                },
+                "required": ["bit_depth"],
+            },
+        },
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "gain_effect",
+            "description": "Add a gain effect to the audio.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "gain_db": {
+                        "type": "number",
+                        "description": "The gain in decibels.",
+                    }
+                },
+                "required": ["gain_db"],
+            },
+        },
+    },
+]
 
 
-def call_music_agent(text: str):
-    llm = ChatMistralAI(
-        model="mistral-large-latest",
-        api_key=api_key,
-    )
+def get_pedal_effects_from_text(text):
+    print(text)
 
-    suggested_effects = []
-
-    @tool
-    def reverb(
-        # room_size: float = 0.5,
-        # damping: float = 0.5,
-        # wet_level: float = 0.33,
-        # dry_level: float = 0.4,
-        # width: float = 1.0,
-        # freeze_mode: float = 0.0,
-    ):
-        "Add a reverb effect to the audio."
-        reverb_effect = Reverb(
-            # room_size=room_size,
-            # damping=damping,
-            # wet_level=wet_level,
-            # dry_level=dry_level,
-            # width=width,
-            # freeze_mode=freeze_mode,
-        )
-        suggested_effects.append(reverb_effect)
-
-    @tool
-    def gain(gain_db: float = 0.1):
-        "Add a gain effect to the audio."
-        gain_effect = Gain(gain_db)
-        suggested_effects.append(gain_effect)
-
-    @tool
-    def bitcrush(bit_depth: int = 8):
-        "Add a bitcrush effect to the audio."
-        bitcrush_effect = Bitcrush(bit_depth=bit_depth)
-        suggested_effects.append(bitcrush_effect)
-
-    tools = [
-        reverb,
-        gain,
-        bitcrush,
+    messages = [
+        ChatMessage(
+            role="system",
+            content="You are a music assistant with access to a music track. You must use function calling!",
+        ),
+        ChatMessage(
+            role="user",
+            content=text,
+        ),
     ]
 
-    prompting_agent = hub.pull("hwchase17/openai-tools-agent")
-    prompting_agent.pretty_print()
-    agent = create_tool_calling_agent(llm, tools, prompting_agent)
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    from pedalboard import Reverb, Gain, Bitcrush
 
-    agent_invoke_result = agent_executor.invoke({"input": text})
-    return (agent_invoke_result, suggested_effects)
+    array = []
+
+    def wrapper(func, **kwargs):
+        array.append(func(**kwargs))
+
+    import functools
+
+    names_to_functions = {
+        "reverb_effect": functools.partial(wrapper, Reverb),
+        "bitcrush_effect": functools.partial(wrapper, Bitcrush),
+        "gain_effect": functools.partial(wrapper, Gain),
+    }
+
+    client = MistralClient(api_key=api_key)
+    response = client.chat(
+        model=model, messages=messages, tools=tools, tool_choice="any", temperature=0.1
+    )
+    import json
+
+    print(response.choices[0].message.content)
+
+    for tool_call in response.choices[0].message.tool_calls:
+        function_name = tool_call.function.name
+        function_params = json.loads(tool_call.function.arguments)
+        print(function_name, function_params)
+        function_result = names_to_functions[function_name](**function_params)
+
+    return array
 
 
-def AI_alter_sound(prompt_text: str):
-    agent_invoke_result, suggested_effects = call_music_agent(prompt_text)
-    suggested_pedalboard = Pedalboard(suggested_effects)
-    print(suggested_effects)
-    return suggested_pedalboard
-
-
-sound_effects = AI_alter_sound(
+effects = get_pedal_effects_from_text(
+    "Add effects to this song to make it sound like it's played in a cathedral."
+)
+print(effects)
+effects = get_pedal_effects_from_text(
     "Add effects to this song to make it sound like it's underwater."
 )
-
-# sound_effects = AI_alter_sound(
-#     "Add effects to this song to make it sound like it's played in a cave."
-# )
+print(effects)
