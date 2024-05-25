5d9026175cb975a2b372fcc48a785e6e62656894
Sriram Vadlamani <sriram.vadlamani@proton.me>
2024-05-25 12:02:53 +0200

    Add initial template for main backend

---
+++ api/main.py
@@ -0,0 +1,24 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def home():
+    return "Hello world!"
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    uvicorn.run(app, host="0.0.0.0", port=4000)
